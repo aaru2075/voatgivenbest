f7abe933b5cb7b99eecfc226b03da01a9f447020
Aaru <aryarai1569@protonmail.com>
2024-10-14 01:37:47 +0530

    config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "3257596471c6d08212b3c0a47cc815ea",
+  "API_HASH": "a1bcb2e0191095a0758e29280cf55933",
   # Get From my.telegram.org
-  "API_ID": "23578854",
+  "API_ID": "23678651",
   #Get For @BotFather
-  "BOT_TOKEN": "7047434941:AAFME3KIhKuKq2gTGc0RG1nTGLSiXk99nxI",
+  "BOT_TOKEN": "8049998303:AAHy2_GdJFep9wqhTzd_exDZ_lqY_YbxqUQ",
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "postgresql://postgres:tTvmOOxCA4TBnFOR@ashamedly-large-feline.data-1.use1.tembo.io:5432/postgres",
+  "DATABASE_URL_PRIMARY": "postgresql://postgres:rhf04OpB0SbaE2Yw@inversely-immaculate-oryx.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "-1002231355848",
+  "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
-  "CHANNEL": "nameless_botz",
+  "CHANNEL": "",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "[{chap_num}] [MW] {chap_name} [@Manhwa_Weebs]"
+  "FNAME": "[MC] [{chap_num}] {chap_name} @Manga_Campus"
 }
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
